3ea97cec423c9bf8b8acb4f15cf7b6dda3618691
Carlos Hermida <djcarloshermida@outlook.com>
2022-12-19 23:59:10 -0300

    Nav

--- src/App.js
+++ src/App.js
@@ -5,20 +5,40 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          página <code>en</code> construcción.
-        </p>
-        <h1>PARA FIESTAS</h1>
-        <a
-          className="App-link"
-          href="https://djcarloshermida.com.uy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          | DJ CARLOS HERMIDA |
-        </a>
+        <nav>
+          <img src={logo} className="App-logo" alt="logo" />
+        </nav>
+        <ul>
+        <a href="index.html">
+            Inicio
+          </a>
+        </ul>
+        <ul>Servicios</ul>
+        <ul>Galería
+          <ol>Fotos</ol>
+          <ol>Videos</ol>
+        </ul>
+        <ul>Multimedia
+          <ol>Curso DJ</ol>
+          <ol>Música Electrónica</ol>
+          <ol>Remix</ol>
+          <ol>Directos</ol>
+        </ul>
+        <ul>Contacto</ul>
       </header>
+      
+
+      <body>
+        <div>
+          <h1>PARA FIESTAS</h1>
+        </div>
+
+      <footer>
+        <a className="App-link" href="https://djcarloshermida.com.uy" target="_blank" rel="noopener noreferrer">
+            | DJ CARLOS HERMIDA |
+          </a>
+      </footer>
+      </body>
     </div>
   );
 }

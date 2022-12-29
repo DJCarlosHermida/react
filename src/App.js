a89983dc4287818a336b84d844e9fbb79c24a2e1
DJCarlosHermida <djcarloshermida@outlook.com>
2022-12-29 14:00:45 -0300

    actualización NavBar SCSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './discoVinilo.png';
 import './App.css';
 import { Footer } from './footer';
 import { OpcionesDisco } from './components/OpcionesDisco';
+import './css/contenedor.scss'
+
 
 function App() {
 
@@ -10,11 +12,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <nav >
-          <ul className="navBar">
-            <ol><a href="#" className="navLink">Inicio</a></ol>
-            <ol><a href="#" className="navLink">Servicios</a></ol>
-            <ol><a href="#" className="navLink">Contacto</a></ol>
-            <img src="https://w7.pngwing.com/pngs/1008/303/png-transparent-shopping-cart-icon-product-return-shopping-cart-retail-supermarket-objects-thumbnail.png" alt="Carrito" title="Carrito" className="carrito"></img>
+          <ul className="header__container">
+          
+            <ol><a href="#" className="header__nav">Inicio</a></ol>
+            <ol><a href="#" className="header__nav">Servicios</a></ol>
+            <ol><a href="#" className="header__nav">Productos</a></ol>
+            <ol><a href="#" className="header__nav">Contacto</a></ol>
+            <button className="material-symbols-outlined header__nav">garden_cart</button>
           </ul>
         </nav>
       </header>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,6 @@ import './Navbar.scss'
 export const Navbar = () => {
 
     return (
-        
-            
-                
-
                 <nav className="header__nav">
                     <a className="header__link" href="#">Enlace 1</a>
                     <a className="header__link" href="#">Enlace 2</a>

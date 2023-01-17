2cbd6234df689e45eaefe792cd3bd629df3b737e
DJCarlosHermida <djcarloshermida@outlook.com>
2023-01-17 11:33:40 -0300

    re iniciando la app

---
+++ appdj/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DJ ECOMMERCE
+        </p>
+        <a
+          className="App-link"
+          href="https://djcarloshermida.com.uy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          | DJCarlos Hermida - Official |
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

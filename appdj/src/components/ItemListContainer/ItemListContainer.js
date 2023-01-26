069c28aecdb3297e8727a3509a7fc4bcc5e998fb
DJCarlosHermida <djcarloshermida@outlook.com>
2023-01-26 09:27:28 -0300

    Asincronía, Promesas

--- appdj/src/components/ItemListContainer/ItemListContainer.js
+++ appdj/src/components/ItemListContainer/ItemListContainer.js
@@ -1,26 +1,40 @@
+import { useEffect, useState } from "react"
+
 const ItemListContainer = () => {
-    const pedirDatos = (bool) => {
-        return new Promise (( resolve, reject) => {
-            if (bool){
-            setTimeout(() => {
-                resolve('promesa resuelta')
-                reject('rechazada')
-            }, 1500)
+        const [conuter, setCounter] = useState(1)
+        const pedirDatos = (bool) => {
+            return new Promise ((resolve, reject) => {
+                setTimeout( () => {
+                if (bool){
+                    resolve('promesa resuelta')
+                } else{
+                    reject('rechazada')
+                }
+                }, 1500)
+            })
         }
-        })
-    }
-pedirDatos(true)
+
+        const handleClick = () => {
+            setCounter(conuter + 1)
+        }
+
+
+
+useEffect (() => {
+    pedirDatos(true)
     .then( (res) =>{
         console.log(res);
     })
     .catch((err) => {
         console.log(err);
-    })
+    })    
+}, [])
     
     return (
         <div className="container my-5">
             <h3>Productos</h3>
             <hr/>
+            <button onClick={handleClick}>Click</button>
         </div>
     )
 }

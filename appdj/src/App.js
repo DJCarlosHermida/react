73dd24acffee45ab76d9b657bdc1e91b48c49400
DJCarlosHermida <djcarloshermida@outlook.com>
2023-01-27 14:07:24 -0300

    MAP

--- appdj/src/App.js
+++ appdj/src/App.js
@@ -1,8 +1,8 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Footer } from "./components/Footer/Footer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import { Footer } from "./components/Footer/Footer";
 
 
 
--- appdj/src/components/ItemListContainer/ItemListContainer.js
+++ appdj/src/components/ItemListContainer/ItemListContainer.js
@@ -1,40 +1,49 @@
 import { useEffect, useState } from "react"
+import MOCK_DATA from '../../data/MOCK_DATA.json'
+import './ItemListContainer.scss'
 
 const ItemListContainer = () => {
-        const [conuter, setCounter] = useState(1)
-        const pedirDatos = (bool) => {
-            return new Promise ((resolve, reject) => {
-                setTimeout( () => {
-                if (bool){
-                    resolve('promesa resuelta')
-                } else{
-                    reject('rechazada')
-                }
-                }, 1500)
+
+    const [productos, setProductos] = useState([])
+    console.log(productos);
+
+    const pedirDatos = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(MOCK_DATA)
+            }, 1500)
+        })
+    }
+
+    useEffect(() => {
+        pedirDatos()
+            .then((res) => {
+                setProductos(res)
             })
-        }
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [])
 
-        const handleClick = () => {
-            setCounter(conuter + 1)
-        }
-
-
-
-useEffect (() => {
-    pedirDatos(true)
-    .then( (res) =>{
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log(err);
-    })    
-}, [])
-    
     return (
         <div className="container my-5">
-            <h3>Productos</h3>
-            <hr/>
-            <button onClick={handleClick}>Click</button>
+            <div className="">
+                <h2 className="seccionTitle">Productos</h2>
+                <hr/>
+                <div className="card">
+
+                { productos.map((productos =>(
+                    <div className="card">
+                        <h3 className="prodcutosName">{productos.name}</h3>
+                        <img className="image" alt="Parlante" title={productos.name} src={productos.img}/>
+                        <p className="description">{productos.description}</p>
+                        <p className="price"><b>USD {productos.price}</b></p>
+                    </div>
+
+)))
+}
+</div>
+            </div>
         </div>
     )
 }
